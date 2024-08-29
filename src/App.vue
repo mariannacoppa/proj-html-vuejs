7332f43a315a2c33379ae4227ce4f22949b6a371
mariannacoppa <densimetra@gmail.com>
2024-08-29 17:18:11 +0200

    [FEAT] installed bootstrap, fontawesome and loaded images

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@ export default {
 }
 </script>
 <template>
-  <div>
-    
-  </div>
+  <h1>Getting Started with Vue.js and Font Awesome</h1>
+  <p>Have a phone call: <font-awesome-icon icon="phone" /></p>
+  <!-- <span class="fa fa-auto"></span> -->
+  <font-awesome-icon icon="facebook" />
 </template>
-<style lang="">
-  
+<style lang="scss">
+  @use './styles/generals.scss';
 </style>
